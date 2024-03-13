fac60d192cc5e3290b03d75a6bc10aaa0432b96d
kadray <1janekguz1@gmail.com>
2024-03-13 15:19:25 +0100

    encryption and database modifications functional

--- CryptModule.py
+++ CryptModule.py
@@ -1,76 +1,46 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Util.Padding import pad, unpad
-import csv
-import io
+import json
 
-# Function to encrypt CSV data and write it to a file
-def encrypt_data(data, output_file, password):
-    # Convert data to CSV format
-    csv_data = io.StringIO()
-    writer = csv.writer(csv_data)
-    writer.writerows(data) #tu zmieniłem
-    csv_data.seek(0)
-    csv_bytes = csv_data.read().encode()
+def decrypt(encrypted_data, key):
+    decrypted_data = []
+    key_length = len(key)
+    for row in encrypted_data:
+        decrypted_row = []
+        for item in row:
+            decrypted_item = ''
+            for char in item:
+                key_char = key[len(decrypted_item) % key_length]
+                decrypted_char = chr(((ord(char) - ord(key_char)) % 256))
+                decrypted_item += decrypted_char
+            decrypted_row.append(decrypted_item)
+        decrypted_data.append(decrypted_row)
+    return decrypted_data
 
-    # Generate a key from the password using PBKDF2
-    salt = get_random_bytes(16)
-    key = PBKDF2(password, salt, dkLen=32, count=1000000)
+def encrypt(data, key):
+    encrypted_data = []
+    key_length = len(key)
+    for row in data:
+        encrypted_row = []
+        for item in row:
+            encrypted_item = ''
+            for char in item:
+                key_char = key[len(encrypted_item) % key_length]
+                encrypted_char = chr(((ord(char) + ord(key_char)) % 256))
+                encrypted_item += encrypted_char
+            encrypted_row.append(encrypted_item)
+        encrypted_data.append(encrypted_row)
+    return encrypted_data
 
-    # Initialize AES cipher in CBC mode
-    cipher = AES.new(key, AES.MODE_CBC)
+def load_and_decrypt_from_json(filename, key):
+    with open(filename, 'r') as file:
+        encrypted_data = json.load(file)
+        decrypted_data = decrypt(encrypted_data, key)
+    return decrypted_data
 
-    # Write the salt and IV to the beginning of the output file
-    with open(output_file, 'wb') as f:
-        f.write(salt)
-        f.write(cipher.iv)
+def encrypt_and_save_to_json(filename, key, data_to_encrypt):
+    encrypted_data = encrypt(data_to_encrypt, key)
+    with open(filename, 'w') as file:
+        json.dump(encrypted_data, file)
 
-        # Pad the data to fit the block size of AES
-        padded_data = pad(csv_bytes, AES.block_size)
-
-        # Encrypt the data
-        encrypted_data = cipher.encrypt(padded_data)
-        f.write(encrypted_data)
-
-# Function to decrypt a CSV file and return the data as a list
-def decrypt_csv(input_file, password):
-    # Open the input file and read the salt and IV
-    with open(input_file, 'rb') as f:
-        salt = f.read(16)
-        iv = f.read(16)
-
-        # Generate key from the password and salt using PBKDF2
-        key = PBKDF2(password, salt, dkLen=32, count=1000000)
-
-        # Initialize AES cipher in CBC mode
-        cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-
-        # Read the encrypted data from the file
-        encrypted_data = f.read()
-
-        # Decrypt the data
-        decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
-
-        # Convert decrypted bytes to CSV format
-        decrypted_str = decrypted_data.decode()
-        csv_data = io.StringIO(decrypted_str)
-        reader = csv.reader(csv_data)
-        decrypted_list = list(reader)
-
-        return decrypted_list
-
-
-# Example usage:
-'''
-data_to_encrypt = [
-    ['Name', 'Age', 'Country'],
-    ['John', '25', 'USA'],
-    ['Alice', '30', 'Canada']
-]
-
-encrypt_data(data_to_encrypt, 'encrypted.csv', 'mypassword')
-decrypted_data = decrypt_csv('encrypted.csv', 'mypassword')
-encrypt_data(decrypt_csv('encrypted.csv', "mypassword"),"test.csv", "mypassword")
-print(decrypted_data)
-'''
+def initialize_json_file(filename):
+    with open(filename, 'w') as file:
+        json.dump([], file)
--- DataStore.py
+++ DataStore.py
@@ -1,59 +1,37 @@
-import csv
-import os
+import json
 import CryptModule as cm
+import os
 
-# input -> nazwa bazy danych, LISTA danych do wpisania | output-> wpisanie danych do kolejnego wiersza
-def write_to_database(db_name, datalist):
-    file="databases/"+db_name+".csv"
-    with open(file, mode='a', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(datalist)
-        csv_file.close()
+class Database:
+    def __init__(self, db_name, pwd):
+        self.name=db_name
+        self.pwd=pwd
 
+    def create_database(self):
+        file=self.name+".json"
+        cm.initialize_json_file(file)
+        credentials= [self.name, self.pwd]
+        cm.encrypt_and_save_to_json(file, self.pwd, credentials)
+        
+    def write_to_database(self, datalist):
+        file=self.name+".json"
+        # Wczytanie i deszyfrowanie danych z pliku JSON
+        decrypted_data = cm.load_and_decrypt_from_json(file, self.pwd)
+        # Dodanie nowego wiersza do danych
+        new_row = datalist
+        decrypted_data.append(new_row)
+        #print(decrypted_data[6])
+        # Szyfrowanie i zapisywanie danych do pliku JSON
+        cm.encrypt_and_save_to_json(file, self.pwd, decrypted_data)
 
-# input -> nazwa bazy danych, index wiersza do usunięcia | output -> usunięcie wpisu i przesunięcie wszystkiego aby zapełnić dziure
-def remove_from_database(db_name, index):
-    # Wczytaj dane z pliku CSV
-    file="databases/"+db_name+".csv"
-    with open(file, 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        data = list(reader)
-    # Usuń wiersz o określonym indeksie
-    del data[index]
-    # Zapisz zaktualizowane dane z powrotem do pliku CSV
-    with open(file, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows(data)
-        csv_file.close()
-
-
-# input -> nazwa bazy danych do utworzenia | output -> utworzenie pliku csv
-def create_database(db_name, pwd):
-        file="databases/"+db_name+".csv"
-        if not os.path.exists(file):
-            # Plik nie istnieje, więc zostanie utworzony
-            with open(file, 'w', newline='') as csv_file:
-                csv_file.close()
-            cm.encrypt_data([db_name, pwd], file, pwd)
-
-def add_record_to_encrypted_csv(input_file, output_file, password, new_record):
-    # Decrypt the encrypted CSV file to get the existing data
-    existing_data = cm.decrypt_csv(input_file, password)
-    
-    # Add the new record to the existing data
-    existing_data.append(new_record)
-    
-    # Encrypt the updated data and write it to the output file
-    cm.encrypt_data(existing_data, output_file, password)
-
-def get_data(input_file, password):
-    decrypted_list= cm.decrypt_csv(input_file, password)
-    strings = [''.join(inner_list) for inner_list in decrypted_list]
-    result = ' '.join(strings)
-    return result
-create_database("baza", "haselko")
+    def read_from_database(self):
+        file=self.name+".json"
+        data=cm.load_and_decrypt_from_json(file, self.pwd)
+        data=data[2:]
+        return data
 
 
-add_record_to_encrypted_csv("databases/baza.csv", "databases/baza.csv", "haselko", ["login2", "haslo2"])
-print(cm.decrypt_csv("databases/baza.csv", "haselko"))
-print(get_data("databases/baza.csv", "haselko"))
+test=Database("pls", "PAsswords_23^")  
+#test.create_database()
+test.write_to_database(["haselko", "tftft","git.pl"])
+print(test.read_from_database())
